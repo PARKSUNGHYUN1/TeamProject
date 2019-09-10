8098f2a179ee66aebba9b35cba4064b4c2f8bc04
SEONMIN99 <seonmin99@naver.com>
2019-09-10 21:51:44 +0900

    Circle 클래스 구현

--- base/Circle.java
+++ base/Circle.java
@@ -1,21 +1,26 @@
 package base;
-
-
+import myInterface.Shape;
 /**
  * Write a description of class Circle here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (2018315040 김선민, 2018315034 정철우, 2018315031 박성현)
+ * @version (2019/09/10)
  */
-public class Circle
+public class Circle implements Shape
 {
     int x;
     int y;
     int radius;
-    
     public void Circle(int x, int y, int radius){
         this.x = x;
         this.y = y;
         this.radius = radius;
     }
+    public double getArea(){
+        return PI * radius * radius;
+    }
+    public void draw(){
+        System.out.println("반지름이"+ radius + "인 원입니다.");
+    }
+    
 }
